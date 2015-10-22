7762d9eaf2dec0e561125c68fc89b0e758746dc1
Sean Straus <sstraus21@gmail.com>
2015-10-22 14:56:02 -0500

    Documentation
    Upload documentation to the repository

---
+++ Documentation/Gnoll Raider - Todo list II.py
@@ -0,0 +1,96 @@
+Gnoll Raider To Do List
+
+"""
+KNOWN_BUGS
+	Sword is less than melee range
+"""
+ABILITIES:
+	let players scroll the ability selector like in xenoblade instead of having to 
+	finish the abilities:
+		{
+		1.
+		2.
+		3.
+		4.
+		5.
+		6.
+		7.
+		8.
+		9.
+		10.
+		11.
+		12.
+		13.
+		14.
+		16.
+		17.
+		18.
+		}
+
+LOGBOOK:
+	The Adventure So Far
+	Quests/Chores
+	Profiles: keep tabs on friends and foes you've met in your adventures
+	Bestiary: The wonders of the natural world, like rhinos and stuff.
+	Maps: 
+	Records: The Greatest Raids of all time
+
+QUALITIES & SKILLS:
+	make some of these someday
+	
+ENEMIES:
+	Smart Movement
+	Camp Awareness
+	Start Position (home awareness)
+	Aggro System
+	obstacle awareness & line of sight
+	state based activity
+	Conditions:snare,topple,stun
+	drop items on death
+
+CORE MECHANICS:
+	strafing
+	set usable items to a hotkey
+	find a way to keep dead players from saving as dead
+	keep ghosts from using items while dead
+	save_and_get_off menu
+	inn/beds for ep regen
+	
+ITEMS:
+	make better items:
+		good bandages (cloth bandages)
+		good food (roast beef)
+		revive item? (stretcher/lifesupport kit)
+		a weapon or two
+	pickupable
+	treasure chests
+	coins animate and make noise when picked up
+
+GAME DESIGN ISSUES:
+	Problematic player deaths: dead players have no fun
+	addicting runthroughs vs. extended exploration
+	the witchdoctor and infinite gold
+	no point in making players pay for healing once back at base
+	prevent easy gold grinding: enemies stop giving gold
+	
+QUESTS:
+	The Great Thanksgiving Feast
+	The Great Pumpkin
+	Return of the Rhino
+	
+CINEMATICS:
+	Title Screen Background
+	Opening Cinematic
+	Gargarak's Quest (Thanksgiving feast)
+	Boss Intro
+
+LEVELS:
+	pillarvalley is 25% finished. finish it.
+	graveyard level
+	Umara Savannah
+
+GRAPHICS:
+	character creation UI
+	background for menus
+	rakasta sprite
+	zombie sprite
